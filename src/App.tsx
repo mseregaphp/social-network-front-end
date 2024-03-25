cf831c3880b146374cfe6b492e6c99825ec1a1de
Serhii Malyi <m.serega.php@gmail.com>
2024-03-25 08:09:19 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Button} from "@nextui-org/react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button color="primary">
+        Button
+      </Button>
+    </div>
+  )
+}
+
+export default App
